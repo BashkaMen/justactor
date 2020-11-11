a0505b51b41dcd0313abeb0d941ff4d418d12d79
blowin <dsessence@gmail.com>
2020-11-11 08:02:35 +0300

    optimize BeeApiary

--- JustActors/BeeApiary.cs
+++ JustActors/BeeApiary.cs
@@ -6,34 +6,24 @@ namespace JustActors
     public class BeeApiary
     {
         private readonly IBeeResolver _beeResolver;
-        private readonly ConcurrentDictionary<Type, ConcurrentDictionary<string, IBee>> _state;
+        private readonly ConcurrentDictionary<(Type, string), IBee> _state;
         
         public BeeApiary(IBeeResolver beeResolver)
         {
             _beeResolver = beeResolver;
-            _state = new ConcurrentDictionary<Type, ConcurrentDictionary<string, IBee>>();
+            _state = new ConcurrentDictionary<(Type, string), IBee>();
         }
 
         public void RegisterBee<T>(string id, T bee) where T : IBee
         {
-            _state.AddOrUpdate(typeof(T), t =>
-            {
-                var dict = new ConcurrentDictionary<string, IBee> {[id] = bee};
-                return dict;
-            }, (t, v) =>
-            {
-                v[id] = bee;
-                return v;
-            });
+            _state.AddOrUpdate((typeof(T), id), bee, (tuple, bee1) => bee1);
         }
 
         public T GetBee<T>(string id) where T : IBee
         {
-            if (_state.TryGetValue(typeof(T), out var bees))
-                if (bees.TryGetValue(id, out var bee))
-                    return (T)bee;
-
-
+            if (_state.TryGetValue((typeof(T), id), out var bee))
+                return (T)bee;
+            
             var newBee = _beeResolver.Resolve<T>();
             RegisterBee(id, newBee);
 
