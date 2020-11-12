565947a406963b924479c7d2e321457aabdc9ffb
Dmitriy Bashinskiy <sbmbash@gmail.com>
2020-11-12 19:45:51 +0200

    added di extension

---
+++ JustActors.Microsoft.DependencyInjection/Configure.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace JustActors.Microsoft.DependencyInjection
+{
+    public static class Configure
+    {
+        public static void AddJustActors(this IServiceCollection services)
+        {
+            services.AddTransient<DIBeeResolver>();
+            services.AddSingleton<BeeApiary>();
+        }
+    }
+}
---
+++ JustActors.Microsoft.DependencyInjection/DIBeeResolver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JustActors.Microsoft.DependencyInjection
+{
+    public class DIBeeResolver : IBeeResolver
+    {
+        private readonly IServiceProvider _provider;
+
+        public DIBeeResolver(IServiceProvider provider)
+        {
+            _provider = provider;
+        }
+        
+        public T Resolve<T>() where T : IBee
+        {
+            return _provider.GetRequiredService<T>();
+        }
+    }
+}
